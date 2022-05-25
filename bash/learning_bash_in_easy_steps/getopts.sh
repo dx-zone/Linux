a21aea0e95c55e05d245a511772f9753c0e142f6
dx-zone <dx.zone@gmail.com>
2022-05-25 17:49:34 -0400

    adding getopts.sh and ../../.gitignore

---
+++ bash/learning_bash_in_easy_steps/getopts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function process_options
+{
+  case $option in
+  a ) echo "Selected $option" ;;
+  b ) echo "Selected $option with argument: $OPTARG";;
+  c ) echo "Selected $option" ;;
+  * ) echo 'Usage: getopts [-a] [-b arg] [-c]' ;;
+  esac
+}

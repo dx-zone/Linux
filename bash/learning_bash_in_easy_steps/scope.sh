65a90c4ed89282f03d1ac845074b2a329d687e14
dx-zone <dx.zone@gmail.com>
2022-05-26 15:11:05 -0400

    updating scope.sh

--- bash/learning_bash_in_easy_steps/scope.sh
+++ bash/learning_bash_in_easy_steps/scope.sh
@@ -1,4 +1,4 @@
-#!/usr/bin
+#!/bin/bash
 clear
 
 # Global variable scope
@@ -14,14 +14,17 @@ echo -e "Global Integer: $int\n"
 
 function call_me
 {
+  # The 'str' variable will be accesible from the global scope
+  str='This is a string define inside a function'
   # Local variable scope
-  str='Linux'
   local int=200
-  echo "Function paraeters: $1 $2"
+  echo "Function parameters: $1 $2"
   echo -e "Local Integer: $int\n"
 }
 
-call_me 'AI'
+#call_me 'Hey', 'you!'
+call_me 'AI' # we are passing only one parameter but the function can also accept two.
 
-echo "Global String: $str"
+# Global variable scope
+echo "Global String: $str" # This will display the variable 'str' inside the function
 echo "Global Integer: $int"
